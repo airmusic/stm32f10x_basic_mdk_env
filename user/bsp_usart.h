a309e7cfc31fe872c0e3d7cd7e743e4261c30ce1
codecaker <codecaker@outlook.com>
2016-10-15 10:43:08 +0800

    update debug config

--- user/bsp_usart.h
+++ user/bsp_usart.h
@@ -4,11 +4,6 @@
 #include "stm32f10x.h"
 #include <stdio.h>
 
-#ifdef DEBUG
-#define DEBUG_PRINTF(format, ...) printf(format, ##__VA_ARGS__)
-#else
-#define DEBUG_PRINTF(format, ...)
-#endif
 
 void usart1_configuration(void);
 static void usart1_nvic_configuration(void);
